570a424ce1562e22c6ce422af1c0c4647eccfd97
Sunhoy-Sun <1139731186@qq.com>
2015-12-16 23:19:16 +0800

    Seven 框架搭建；不知道jar上传过去没；数据库的初始处理有个DB文件夹说明

--- src/main/java/com/win/xymz/controller/HomeController.java
+++ src/main/java/com/win/xymz/controller/HomeController.java
@@ -3,9 +3,6 @@ package com.win.xymz.controller;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+public class HomeController extends BaseController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -28,9 +22,7 @@ public class HomeController {
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
-		
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,7 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Home</title>
 </head>
 <body>
@@ -10,5 +13,11 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<p><a href="javascript:void(0)" onclick="goLogin()">Go Login</a></p>
 </body>
+<script type="text/javascript">
+	function goLogin(){
+		window.location.href="./test.do?method=toLoginTestPage";
+	}
+</script>
 </html>
