8f22ce2be16ed88f2fd7b7f1af8b5c609f42a8e4
CarlZhang <carlzhangweiwen@sina.com>
2015-12-23 21:41:15 +0800

    架构调整

---
+++ src/main/java/com/win/xymz/bean/User.java
@@ -0,0 +1,30 @@
+package com.win.xymz.bean;
+/** 
+ * @author 作者 Carl Zhang. E-mail: carlzhangweiwen@sina.com
+ * @version 创建时间：2015年12月23日 下午8:35:33 
+ * 类说明 
+ */
+public class User {
+	private String username;//用户名
+	private String password;//密码
+	private String mobilephone;//手机号
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobilephone() {
+		return mobilephone;
+	}
+	public void setMobilephone(String mobilephone) {
+		this.mobilephone = mobilephone;
+	}
+}
+
---
+++ src/main/java/com/win/xymz/controller/user/UserInfoController.java
@@ -0,0 +1,19 @@
+package com.win.xymz.controller.user;
+
+import org.springframework.stereotype.Controller;
+
+/** 
+ * @author 作者 Carl Zhang. E-mail: carlzhangweiwen@sina.com
+ * @version 创建时间：2015年12月23日 下午9:16:02 
+ * 类说明 
+ */
+@Controller
+public class UserInfoController {
+	
+	public String login(){
+		
+		return "";
+	}
+	
+}
+
